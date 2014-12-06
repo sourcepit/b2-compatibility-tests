a7b524467ff65e83f306d00d1a56a0703a093528
Bernd Vogt <bernd.vogt@sourcepit.org>
2014-12-06 12:25:22 +0100

    Added dummy unit tests

--- src/test/resources/org.sourcepit.b2compat/org.sourcepit.b2compat.ui/src/org/sourcepit/b2compat/ui/Activator.java
+++ src/test/resources/org.sourcepit.b2compat/org.sourcepit.b2compat.ui/src/org/sourcepit/b2compat/ui/Activator.java
@@ -1,50 +1,75 @@
+/*
+ * Copyright 2014 Bernd Vogt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.sourcepit.b2compat.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author Bernd Vogt <bernd.vogt@sourcepit.org>
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin
+{
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.sourcepit.b2compat.ui"; //$NON-NLS-1$
+   // The plug-in ID
+   public static final String PLUGIN_ID = "org.sourcepit.b2compat.ui"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+   // The shared instance
+   private static Activator plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+   /**
+    * The constructor
+    */
+   public Activator()
+   {
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+    */
+   public void start(BundleContext context) throws Exception
+   {
+      super.start(context);
+      plugin = this;
+   }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+    */
+   public void stop(BundleContext context) throws Exception
+   {
+      plugin = null;
+      super.stop(context);
+   }
+
+   /**
+    * Returns the shared instance
+    *
+    * @return the shared instance
+    */
+   public static Activator getDefault()
+   {
+      return plugin;
+   }
 
 }
